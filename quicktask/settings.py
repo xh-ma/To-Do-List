f11492c7b412f8fc62a9a159c41b78a63f9848b7
Xiaohan Ma <xiaohan.m9@gmail.com>
2025-05-15 21:59:19 -0400

    Update settings.py

--- quicktask/settings.py
+++ quicktask/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
